bb2cc22da306bb0107a5960f806ad64f0673f9f0
MWasile <maaciej.wasilewski@gmail.com>
2024-09-28 22:29:53 +0200

    maybe this ;

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,12 +1,19 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       'src': '/src'
     }
-  }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+  },
 })
