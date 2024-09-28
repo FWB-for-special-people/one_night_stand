410f61e748517c6ab50ef011bbd579a7d9ac44f1
Martyna Witkowska <mar.witkowska@gmail.com>
2024-09-28 13:20:59 +0200

    front

---
+++ app/src/App.tsx
@@ -0,0 +1,12 @@
+import {Typography} from "@mui/material";
+
+function App() {
+
+  return (
+      <Typography variant="h1" color="textPrimary">
+          hello!
+      </Typography>
+  )
+}
+
+export default App
