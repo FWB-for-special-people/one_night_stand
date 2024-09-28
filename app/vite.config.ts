77a9f9dece8db2e66b3b9eaaac06a2ddbf5f4317
MWasile <maaciej.wasilewski@gmail.com>
2024-09-28 22:35:44 +0200

    back to production build

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,19 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       'src': '/src'
     }
-  },
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-  },
-  build: {
-    outDir: 'dist',
-  },
+  }
 })
