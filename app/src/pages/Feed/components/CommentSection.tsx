76173b6f973b2b75672fd0226f187d47170caa24
PatrykBombik <patryk.bombik@gmail.com>
2024-09-29 09:10:00 +0200

    comments adding done

--- app/src/pages/Feed/components/CommentSection.tsx
+++ app/src/pages/Feed/components/CommentSection.tsx
@@ -11,32 +11,45 @@ interface CommentsSectionProps {
 interface Comment {
   id: number;
   text: string;
-  createdBy: string;
+  createdBy: number;
+  createdAt: string;
+}
+
+function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Miesiące są indeksowane od 0
+  const year = date.getFullYear();
+
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 }
 
 const CommentsSection: React.FC<CommentsSectionProps> = ({ postId }) => {
   const [newComment, setNewComment] = useState('');
   const axios = useAxios();
 
-  // Pobieramy komentarze przy użyciu custom hooka
   const { data: commentsData, refetch } = useCardCommentsQuery(postId);
 
-  // Mapped comments przetwarza dane z API
   const mappedComments = commentsData?.pages?.flatMap(page =>
     page.results.map(comment => ({
       id: comment.id,
       text: comment.text,
       createdBy: comment.created_by,
-    }))
+      createdAt: comment.created_at,
+    })) as Comment[],
   );
 
   const handleAddComment = async () => {
     try {
       const response = await axios.post(API.cardComments(postId), { text: newComment });
       if (response.status === 201) {
-        // Po dodaniu komentarza, odświeżamy listę komentarzy
         refetch();
-        setNewComment(''); // Czyścimy pole tekstowe
+        setNewComment('');
       }
     } catch (error) {
       console.error('Error adding comment:', error);
@@ -49,20 +62,26 @@ const CommentsSection: React.FC<CommentsSectionProps> = ({ postId }) => {
         marginTop: '1rem',
         padding: '1rem',
         backgroundColor: 'background.paper',
-        borderRadius: '.5rem',
+        borderRadius: '.25rem',
         width: '100%',
+        border: '.5px solid black',
       }}
     >
-      <Typography variant="h6" sx={{ marginBottom: '1rem' }}>
+      <Typography
+        sx={{
+          marginBottom: '1rem',
+          fontWeight: 'bold',
+          fontSize: '1.2rem',
+        }}>
         Komentarze
       </Typography>
 
-      {/* Formularz do dodania komentarza */}
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-end', gap: '1rem', marginBottom: '1rem' }}>
         <TextField
           fullWidth
           label="Dodaj komentarz"
           variant="outlined"
+          sx={{ backgroundColor: 'background.default' }}
           value={newComment}
           onChange={(e) => setNewComment(e.target.value)}
         />
@@ -71,21 +90,51 @@ const CommentsSection: React.FC<CommentsSectionProps> = ({ postId }) => {
         </Button>
       </Box>
 
-      {/* Lista komentarzy */}
       {mappedComments?.length ? (
         mappedComments.map((comment) => (
           <Box
             key={comment.id}
             sx={{
               padding: '.5rem',
-              borderBottom: '1px solid rgba(0,0,0,0.1)',
+              border: '.5px solid black',
+              borderRadius: '.25rem',
               marginBottom: '.5rem',
+              backgroundColor: 'background.default',
             }}
           >
-            <Typography variant="body1">{comment.text}</Typography>
-            <Typography variant="caption" sx={{ color: 'text.secondary' }}>
-              {comment.createdBy}
-            </Typography>
+            <Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                }}>
+                <Typography
+                  variant="caption"
+                  sx={{
+                    color: 'primary.main',
+                    borderBottom: '.5px solid black',
+                    fontSize: '.5rem',
+                  }}>
+                  {comment.createdBy}
+                </Typography>
+                <Typography
+                  variant="caption"
+                  sx={{
+                    color: 'primary.main',
+                    borderBottom: '.5px solid black',
+                    fontSize: '.5rem',
+                  }}>
+                  {formatDate(comment.createdAt)}
+                </Typography>
+              </Box>
+              <Typography
+                sx={{
+                  marginTop: '.5rem',
+                  fontWeight: 'bold',
+                  fontSize: '.8rem',
+                }}>{comment.text}</Typography>
+            </Box>
           </Box>
         ))
       ) : (
